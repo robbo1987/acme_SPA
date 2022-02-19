9aa8a533d03a3b789d5774998511ea278f120ef8
Robert Albenda <ralbenda@gmail.com>
2022-02-19 11:39:08 -0500

    phase 2

--- server.js
+++ server.js
@@ -3,15 +3,35 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost/acme_spa"
 );
 
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
 const Things = sequelize.define("thing", {
   name: {
     type: Sequelize.DataTypes.STRING,
   }
 });
 
+
+
+
+app.get( '/api/things', async(req,res,next) => {
+try{
+     res.send(await Things.findAll())
+
+}
+
+catch(ex) {
+  next(ex)
+}
+})
 const init = async () => {
   try {
-    console.log("hello world");
+    console.log("data seeded");
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    } )
     await sequelize.sync({ force: true });
     await Things.create({ name: "foo" });
     await Things.create({ name: "bar" });
